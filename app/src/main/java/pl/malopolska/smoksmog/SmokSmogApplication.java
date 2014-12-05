f77fd9661cb595a29603bccd15383cd5a33fb5b3
Iwo Polański <outlying@gmail.com>
2014-12-05 11:57:23 +0100

    Added Crashlytics for future development

--- app/src/main/java/pl/malopolska/smoksmog/SmokSmogApplication.java
+++ app/src/main/java/pl/malopolska/smoksmog/SmokSmogApplication.java
@@ -1,9 +1,16 @@
 package pl.malopolska.smoksmog;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Project Application class
  */
 public class SmokSmogApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());    }
+
 }
