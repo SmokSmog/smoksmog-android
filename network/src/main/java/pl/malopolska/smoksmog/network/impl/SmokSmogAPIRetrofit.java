4f10f0a1054bd91358943037798d0acf537aeca4
Iwo Polański <outlying@gmail.com>
2014-12-17 16:35:50 +0100

    Correction

--- network/src/main/java/pl/malopolska/smoksmog/network/impl/SmokSmogAPIRetrofit.java
+++ network/src/main/java/pl/malopolska/smoksmog/network/impl/SmokSmogAPIRetrofit.java
@@ -4,12 +4,14 @@ import java.util.Collection;
 
 import pl.malopolska.smoksmog.network.SmokSmogAPI;
 import pl.malopolska.smoksmog.network.StationState;
+import retrofit.http.GET;
 
 /**
- * Overrides some of SmokSmogAPI methods to provide capability with Retrofit REST client generator/
+ * Overrides some of SmokSmogAPI methods to provide Retrofit REST client.
  */
 public interface SmokSmogAPIRetrofit extends SmokSmogAPI {
 
     @Override
+    @GET( "/" )
     Collection<StationState> requestStations();
 }
