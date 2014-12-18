f665c7e4ea2f18050f86776abf01b03a02264568
Iwo Polański <outlying@gmail.com>
2014-12-18 12:43:57 +0100

    Dagger to netwotrk module

--- network/src/main/java/pl/malopolska/smoksmog/network/SmokSmog.java
+++ network/src/main/java/pl/malopolska/smoksmog/network/SmokSmog.java
@@ -2,45 +2,23 @@ package pl.malopolska.smoksmog.network;
 
 import android.content.Context;
 
-import java.util.Locale;
-
-import pl.malopolska.smoksmog.network.impl.SmokSmogAPIRetrofit;
-import retrofit.Endpoint;
-import retrofit.Endpoints;
-import retrofit.RestAdapter;
+import dagger.ObjectGraph;
 
 /**
  *
  */
 public final class SmokSmog {
 
-    /**
-     * Get API implementation.
-     *
-     * @param context required for components
-     * @return SmokSmogAPI object
-     */
-    public static SmokSmogAPI getInstance( Context context, String baseUrl ,Locale locale ){
-
-        // TODO check '/'
-        String url = baseUrl + locale.getLanguage();
-
-        Endpoint endpoint = Endpoints.newFixedEndpoint(url);
-
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint( endpoint )
-                .build();
-
-        return restAdapter.create( SmokSmogAPIRetrofit.class );
-    }
+    private final ObjectGraph objectGraph;
 
     /**
-     * Get API implementation.
      *
-     * @param context required for components
-     * @return SmokSmogAPI object
+     * 
+     * @param context
      */
-    public static SmokSmogAPI getInstance( Context context, String baseUrl ){
-        return getInstance( context, baseUrl, context.getResources().getConfiguration().locale );
+    public SmokSmog( Context context ) {
+
+        objectGraph = ObjectGraph.create( new SmokSmogModule(context) );
+        objectGraph.inject( this );
     }
 }
--- network/src/main/java/pl/malopolska/smoksmog/network/SmokSmogModule.java
+++ network/src/main/java/pl/malopolska/smoksmog/network/SmokSmogModule.java
@@ -1,7 +1,16 @@
 package pl.malopolska.smoksmog.network;
 
+import android.content.Context;
+
+import dagger.Module;
+
 /**
  * Created by iwopolanski on 18.12.14.
  */
+@Module
 public class SmokSmogModule {
+
+    public SmokSmogModule(Context context){
+
+    }
 }
