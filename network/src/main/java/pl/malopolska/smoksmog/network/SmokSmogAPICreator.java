e79415814838db7c3c097df0856ade2a497fbd21
Iwo Polański <outlying@gmail.com>
2014-12-24 22:44:59 +0100

    Update creator an tests

--- network/src/main/java/pl/malopolska/smoksmog/network/SmokSmogAPICreator.java
+++ network/src/main/java/pl/malopolska/smoksmog/network/SmokSmogAPICreator.java
@@ -3,11 +3,16 @@ package pl.malopolska.smoksmog.network;
 import java.util.Locale;
 
 import retrofit.RestAdapter;
+import retrofit.converter.Converter;
 
 /**
- * Created by iwopolanski on 24.12.14.
+ * API creator
  */
 public final class SmokSmogAPICreator {
+
+    private SmokSmogAPICreator() {
+        throw new UnsupportedOperationException( "Utility class" );
+    }
 
     /**
      * Valid way for API creation
@@ -26,12 +31,28 @@ public final class SmokSmogAPICreator {
             throw new IllegalArgumentException( "Locale cannot be null" );
         }
 
+        // TODO check what is the last character of endpoint URL
         String baseUrl = endpoint + "/" + locale.getLanguage();
 
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint( baseUrl )
+                .setConverter( jacksonConverter() )
                 .build();
 
         return restAdapter.create( SmokSmogAPI.class );
+    }
+
+    /**
+     * Prepare Jackson2 Converter for handling JSON responses.
+     *
+     * @return Converter implementation
+     */
+    private static Converter jacksonConverter() {
+
+        Converter converter = null;
+
+        // TODO setup Jackson ObjectMapper
+
+        return converter;
     }
 }
--- network/src/test/java/pl/malopolska/smoksmog/network/SmokSmogAPITest.java
+++ network/src/test/java/pl/malopolska/smoksmog/network/SmokSmogAPITest.java
@@ -2,9 +2,23 @@ package pl.malopolska.smoksmog.network;
 
 import junit.framework.TestCase;
 
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SmokSmogAPITest extends TestCase {
 
-    public void testStations() {
+    private SmokSmogAPI smokSmogAPI;
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        smokSmogAPI = SmokSmogAPICreator.create(
+                "http://api.smoksmog.jkostrz.name/", new Locale( "pl" ) );
+    }
+
+    public void testStations() {
+        assertThat( smokSmogAPI.stations() ).isNullOrEmpty();
     }
 }
