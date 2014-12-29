9ba0394db3d07556b01de41d588908ba3eebf443
Iwo Polański <outlying@gmail.com>
2014-12-30 00:52:32 +0100

    Corrected

--- network/src/test/java/pl/malopolska/smoksmog/network/SmokSmogAPICreatorTest.java
+++ network/src/test/java/pl/malopolska/smoksmog/network/SmokSmogAPICreatorTest.java
@@ -2,19 +2,24 @@ package pl.malopolska.smoksmog.network;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 public class SmokSmogAPICreatorTest extends TestCase {
 
     public static final String ENDPOINT = "http://www.onet.pl";
 
     public void testCreateValid() throws Exception {
-        SmokSmogAPICreator.create( ENDPOINT );
+        SmokSmogAPICreator.create(ENDPOINT);
+        SmokSmogAPICreator.create(ENDPOINT + "/");
     }
 
+    @Test(expected = IllegalArgumentException.class)
     public void testCreateInvalid() throws Exception {
-        SmokSmogAPICreator.create( null, null );
+        SmokSmogAPICreator.create(null, null);
     }
 
+    @Test(expected = IllegalArgumentException.class)
     public void testCreateInvalidSecond() throws Exception {
-        SmokSmogAPICreator.create(ENDPOINT, null );
+        SmokSmogAPICreator.create(ENDPOINT, null);
     }
 }
