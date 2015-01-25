051d3db63fda13d2412020c729b7bbd3c8396887
Iwo Polański <outlying@gmail.com>
2015-01-25 11:39:59 +0100

    Added ButterKnife

---
+++ app/src/main/java/pl/malopolska/smoksmog/BaseActivity.java
@@ -0,0 +1,15 @@
+package pl.malopolska.smoksmog;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.inject( this );
+    }
+}
