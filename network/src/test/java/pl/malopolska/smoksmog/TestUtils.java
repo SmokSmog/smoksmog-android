a89fdda9f32902748d017fac4eadf76e2d6b4640
Iwo Polański <outlying@gmail.com>
2015-08-25 17:58:13 +0200

    Add missing method

--- network/src/test/java/pl/malopolska/smoksmog/TestUtils.java
+++ network/src/test/java/pl/malopolska/smoksmog/TestUtils.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -37,7 +39,18 @@ public class TestUtils {
 
     public static String readFileToString( String path, Charset charset ) throws IOException {
         List<String> lines = Files.readAllLines( Paths.get( path ), charset );
-        return String.join( "", lines );
+        return join("",lines);
     }
 
+    private static String join(String delim, Collection<?> col) {
+        StringBuilder sb = new StringBuilder();
+        Iterator<?> iter = col.iterator();
+        if (iter.hasNext())
+            sb.append(iter.next().toString());
+        while (iter.hasNext()) {
+            sb.append(delim);
+            sb.append(iter.next().toString());
+        }
+        return sb.toString();
+    }
 }
