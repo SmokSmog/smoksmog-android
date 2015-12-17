63db9812f6ff74d0cdd45f05c8d759c95811425b
kamil-zych <kamil.zych@schibsted.pl>
2015-12-17 11:51:30 +0100

    Add OnClick for history button, add charts to history layout, change createIntent method signature (use station ID instead of Station)

--- app/src/main/java/com/antyzero/smoksmog/ui/HistoryActivity.java
+++ app/src/main/java/com/antyzero/smoksmog/ui/HistoryActivity.java
@@ -3,20 +3,38 @@ package com.antyzero.smoksmog.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.antyzero.smoksmog.R;
+import com.antyzero.smoksmog.SmokSmogApplication;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
-import pl.malopolska.smoksmog.model.Station;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import pl.malopolska.smoksmog.SmokSmog;
 
 /**
  * Shows history chart
  */
 public class HistoryActivity extends BaseActivity {
-    private static final String STATION_KEY = "station_key";
+    private static final String STATION_ID_KEY = "station_id_key";
 
-    public static Intent createIntent(final Context context, final Station station) {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.lineChart1)
+    LineChart lineChart1;
+
+    public static Intent createIntent(final Context context, final long stationId) {
         final Intent intent = new Intent(context, HistoryActivity.class);
-        intent.putExtra(STATION_KEY, station);
+        intent.putExtra(STATION_ID_KEY, stationId);
         return intent;
     }
 
@@ -25,5 +43,18 @@ public class HistoryActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+    }
+
+    private void setupChartView(final LineChart lineChart) {
+    }
+
+    /**
+     * @return Station ID if available or throws a {@link IllegalArgumentException}
+     */
+    private static long getStationIdExtra(final Intent intent) {
+        if (intent == null || !intent.hasExtra(STATION_ID_KEY)) {
+            throw new IllegalArgumentException();
+        }
+        return intent.getLongExtra(STATION_ID_KEY, -1);
     }
 }
