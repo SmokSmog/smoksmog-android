a5c3192f132fbaffada77bd0c11a518d318eb415
Iwo Polański <outlying@gmail.com>
2016-01-04 20:50:46 +0100

    Add missing tests

---
+++ app/src/androidTest/java/com/antyzero/smoksmog/HistoryActivityTest.java
@@ -0,0 +1,38 @@
+package com.antyzero.smoksmog;
+
+
+import android.app.Activity;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.antyzero.smoksmog.ui.screen.history.HistoryActivity;
+import com.antyzero.smoksmog.ui.screen.main.MainActivity;
+import com.squareup.spoon.Spoon;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith( AndroidJUnit4.class )
+@LargeTest
+public class HistoryActivityTest {
+
+    @Rule
+    public final MockedNetworkActivityTestRule<HistoryActivity> activityTestRule =
+            new MockedNetworkActivityTestRule<>( HistoryActivity.class );
+
+    @Test
+    @UiThreadTest
+    public void checkCreation() {
+
+        // given
+        Activity activity = activityTestRule.getActivity();
+
+        // when
+        //
+
+        // then
+        Spoon.screenshot( activity, "Created" );
+    }
+}
