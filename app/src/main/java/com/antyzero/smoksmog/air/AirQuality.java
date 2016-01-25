963ab3d75679677009cca55e3b8c4e4c472c088e
Iwo Polański <outlying@gmail.com>
2016-01-25 19:06:17 +0100

    Update

--- app/src/main/java/com/antyzero/smoksmog/air/AirQuality.java
+++ app/src/main/java/com/antyzero/smoksmog/air/AirQuality.java
@@ -1,6 +1,9 @@
 package com.antyzero.smoksmog.air;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.ColorRes;
+import android.support.annotation.StringRes;
 
 import com.antyzero.smoksmog.R;
 
@@ -9,42 +12,42 @@ import com.antyzero.smoksmog.R;
  */
 public enum AirQuality implements ValueCheck {
 
-    VERY_GOOD( R.color.airQualityVeryGood ) {
+    VERY_GOOD( R.color.airQualityVeryGood, R.string.airQualityVeryGood ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value <= 1;
         }
     },
 
-    GOOD( R.color.airQualityGood ) {
+    GOOD( R.color.airQualityGood, R.string.airQualityGood ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value > 1 && value <= 3;
         }
     },
 
-    MODERATE( R.color.airQualityModerate ) {
+    MODERATE( R.color.airQualityModerate, R.string.airQualityModerate ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value > 3 && value <= 5;
         }
     },
 
-    SUFFICIENT( R.color.airQualitySufficient ) {
+    SUFFICIENT( R.color.airQualitySufficient, R.string.airQualitySufficient ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value > 5 && value <= 7;
         }
     },
 
-    BAD( R.color.airQualityBad ) {
+    BAD( R.color.airQualityBad, R.string.airQualityBad ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value > 7 && value <= 10;
         }
     },
 
-    VERY_BAD( R.color.airQualityVeryBad ) {
+    VERY_BAD( R.color.airQualityVeryBad, R.string.airQualityVeryBad ) {
         @Override
         public boolean isValueInRange( double value ) {
             return value > 10;
@@ -52,12 +55,24 @@ public enum AirQuality implements ValueCheck {
     };
 
     private final int colorId;
+    private final int nameId;
 
-    AirQuality( @ColorRes int colorId ) {
+    AirQuality( @ColorRes int colorId, @StringRes int nameId ) {
         this.colorId = colorId;
+        this.nameId = nameId;
     }
 
-    public static final AirQuality findByValue( double value ) {
+    public CharSequence getTitle( Context context ) {
+        return context.getString( nameId );
+    }
+
+    public int getColor( Context context ){
+        // Deprecated since 23 API, quite fresh
+        //noinspection deprecation
+        return context.getResources().getColor( colorId );
+    }
+
+    public static AirQuality findByValue( double value ) {
 
         for ( AirQuality airQuality : values() ) {
 
