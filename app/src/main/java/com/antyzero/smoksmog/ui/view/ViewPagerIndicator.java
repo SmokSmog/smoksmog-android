71a5d3b3c13997aeecbe20fe5baadedce35715d5
Iwo Polański <outlying@gmail.com>
2016-02-05 19:25:24 +0100

    Correct indicator, margin between items

--- app/src/main/java/com/antyzero/smoksmog/ui/view/ViewPagerIndicator.java
+++ app/src/main/java/com/antyzero/smoksmog/ui/view/ViewPagerIndicator.java
@@ -2,7 +2,8 @@ package com.antyzero.smoksmog.ui.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,11 +21,19 @@ public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeLi
 
     private List<Long> stationIds = new ArrayList<>();
 
-    private int itemSideSize = 0;
     private int itemSpacing = 0;
+    private int itemSideSize = 0;
+    private int itemSideHalfSize = 0;
 
     private int desiredWidth = 0;
     private int desiredHeight = 0;
+
+    private Paint paintActiveIndicator;
+    private Paint paintIndicator;
+
+    private Path arrow;
+
+    private int activePosition = Integer.MIN_VALUE;
 
     public ViewPagerIndicator( Context context ) {
         super( context );
@@ -42,12 +51,34 @@ public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeLi
     }
 
     private void init( Context context ) {
-        itemSideSize = context.getResources().getDimensionPixelSize( R.dimen.indicator_item_size );
         itemSpacing = context.getResources().getDimensionPixelSize( R.dimen.indicator_item_spacing );
+        itemSideSize = context.getResources().getDimensionPixelSize( R.dimen.indicator_item_size );
+        itemSideHalfSize = itemSideSize / 2;
+
+        paintActiveIndicator = new Paint();
+        paintActiveIndicator.setColor( getResources().getColor( R.color.iron ) );
+        paintActiveIndicator.setStyle( Paint.Style.FILL );
+        paintActiveIndicator.setStrokeWidth( 0 );
+
+        paintIndicator = new Paint( paintActiveIndicator );
+        paintIndicator.setAlpha( 127 );
+
+        arrow = new Path();
+        arrow.reset();
+        arrow.moveTo( itemSideHalfSize, itemSideHalfSize );
+        arrow.moveTo( itemSideHalfSize, itemSideSize );
+        arrow.moveTo( itemSideSize, 0 );
+        arrow.moveTo( 0, itemSideHalfSize );
+        arrow.close();
     }
 
     public void setStationIds( List<Long> stationIds ) {
         this.stationIds = stationIds;
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
 
         desiredHeight = 0;
         desiredWidth = 0;
@@ -60,18 +91,30 @@ public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeLi
             }
         }
 
-        invalidate();
-    }
-
-    @Override
-    protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
-        setBackgroundColor( Color.CYAN );
         setMeasuredDimension( desiredWidth, desiredHeight );
     }
 
     @Override
     protected void onDraw( Canvas canvas ) {
         super.onDraw( canvas );
+        for ( int i = 0; i < stationIds.size(); i++ ) {
+            Paint paint = paintIndicator;
+
+            if ( i == activePosition ) {
+                paint = paintActiveIndicator;
+            }
+
+            if ( stationIds.get( i ) == 0 ) {
+                canvas.save();
+                canvas.translate( ( itemSideSize + itemSpacing ) * i, 0 );
+                canvas.drawPath( arrow, paint );
+                canvas.restore();
+            } else {
+                int cx = itemSideHalfSize + ( itemSideSize + itemSpacing ) * i;
+                int cy = this.itemSideHalfSize;
+                canvas.drawCircle( cx, cy, this.itemSideHalfSize, paint );
+            }
+        }
     }
 
     @Override
@@ -81,7 +124,11 @@ public class ViewPagerIndicator extends View implements ViewPager.OnPageChangeLi
 
     @Override
     public void onPageSelected( int position ) {
-
+        if ( position != activePosition ) {
+            activePosition = position;
+            postInvalidate();
+            // TODO we may ask to redraw only specific areas ?
+        }
     }
 
     @Override
