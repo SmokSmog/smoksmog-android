ac01e0e7f131bf316a0faa67b064d42115864e09
Iwo Polański <outlying@gmail.com>
2016-02-06 13:11:50 +0100

    Change layout

--- app/src/main/java/com/antyzero/smoksmog/ui/screen/start/item/ParticulateViewHolder.java
+++ app/src/main/java/com/antyzero/smoksmog/ui/screen/start/item/ParticulateViewHolder.java
@@ -1,13 +1,18 @@
 package com.antyzero.smoksmog.ui.screen.start.item;
 
 import android.view.View;
+import android.widget.TextView;
 
+import com.antyzero.smoksmog.R;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import pl.malopolska.smoksmog.model.Particulate;
 
 public class ParticulateViewHolder extends ListViewHolder<Particulate> {
 
-
+    @Bind( R.id.textViewName )
+    TextView textViewName;
 
     public ParticulateViewHolder( View itemView ) {
         super( itemView );
@@ -17,5 +22,6 @@ public class ParticulateViewHolder extends ListViewHolder<Particulate> {
     @Override
     public void bind( Particulate data ) {
 
+        textViewName.setText( data.getName() );
     }
 }
