61068020b2e9cbc71d0e66c854c7c16dd42464f3
Iwo Polański <outlying@gmail.com>
2016-02-11 22:30:49 +0100

    Adding order activity

---
+++ app/src/main/java/com/antyzero/smoksmog/ui/screen/order/OrderActivity.java
@@ -0,0 +1,15 @@
+package com.antyzero.smoksmog.ui.screen.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.antyzero.smoksmog.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_order );
+    }
+}
