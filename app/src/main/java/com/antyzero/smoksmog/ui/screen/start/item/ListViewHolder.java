1a8a84d2739a58c42db525eee8cb8902ea840887
Iwo Polański <outlying@gmail.com>
2016-02-25 08:10:11 +0100

    Remove item animation, buggy

--- app/src/main/java/com/antyzero/smoksmog/ui/screen/start/item/ListViewHolder.java
+++ app/src/main/java/com/antyzero/smoksmog/ui/screen/start/item/ListViewHolder.java
@@ -7,12 +7,8 @@ public abstract class ListViewHolder<T> extends RecyclerView.ViewHolder {
 
     public ListViewHolder( View itemView ) {
         super( itemView );
-        itemView.setAlpha( 0f );
     }
 
     public void bind( T data ) {
-        itemView.animate().alpha( 1f )
-                .setStartDelay( 100L * getLayoutPosition() )
-                .setDuration( 200L ).start();
     }
 }
