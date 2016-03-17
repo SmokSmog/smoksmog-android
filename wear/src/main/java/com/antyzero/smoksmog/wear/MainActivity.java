55a7eb47467187bc4cef58389321b7e411faf838
Iwo Polański <outlying@gmail.com>
2016-03-17 18:44:15 +0100

    Corrections

--- wear/src/main/java/com/antyzero/smoksmog/wear/MainActivity.java
+++ wear/src/main/java/com/antyzero/smoksmog/wear/MainActivity.java
@@ -1,24 +1,30 @@
 package com.antyzero.smoksmog.wear;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private TextView mTextView;
+
+public class MainActivity extends Activity implements WatchViewStub.OnLayoutInflatedListener {
+
+    @Bind( R.id.text )
+    TextView mTextView;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        final WatchViewStub stub = ( WatchViewStub ) findViewById( R.id.watch_view_stub );
-        stub.setOnLayoutInflatedListener( new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated( WatchViewStub stub ) {
-                mTextView = ( TextView ) stub.findViewById( R.id.text );
-            }
-        } );
+        ( ( WatchViewStub ) findViewById( R.id.watch_view_stub ) ).setOnLayoutInflatedListener( this );
+    }
+
+    @Override
+    public void onLayoutInflated( WatchViewStub watchViewStub ) {
+        ButterKnife.bind( this, watchViewStub );
+        mTextView.setText( "asdasd" );
     }
 }
