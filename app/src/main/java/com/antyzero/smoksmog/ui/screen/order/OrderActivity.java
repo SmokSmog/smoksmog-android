41921fc2860f65ad4cfdf2d458753c73d09fc354
Iwo Polański <iwo.polanski@schibsted.pl>
2016-09-15 11:55:46 +0200

    Calls updated

--- app/src/main/java/com/antyzero/smoksmog/ui/screen/order/OrderActivity.java
+++ app/src/main/java/com/antyzero/smoksmog/ui/screen/order/OrderActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import com.antyzero.smoksmog.R;
 import com.antyzero.smoksmog.SmokSmogApplication;
 import com.antyzero.smoksmog.error.ErrorReporter;
-import smoksmog.logger.Logger;
 import com.antyzero.smoksmog.settings.SettingsHelper;
 import com.antyzero.smoksmog.ui.BaseDragonActivity;
 import com.antyzero.smoksmog.ui.screen.ActivityModule;
@@ -32,14 +31,14 @@ import butterknife.Bind;
 import butterknife.OnClick;
 import pl.malopolska.smoksmog.SmokSmog;
 import pl.malopolska.smoksmog.model.Station;
+import pl.malopolska.smoksmog.utils.StationUtils;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import smoksmog.logger.Logger;
 
 import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-import static pl.malopolska.smoksmog.utils.StationUtils.convertStationsToIdsArray;
-import static pl.malopolska.smoksmog.utils.StationUtils.convertStationsToIdsList;
 
 public class OrderActivity extends BaseDragonActivity implements OnStartDragListener, StationDialogAdapter.StationListener {
 
@@ -55,20 +54,20 @@ public class OrderActivity extends BaseDragonActivity implements OnStartDragList
     @Inject
     ErrorReporter errorReporter;
 
-    @Bind( R.id.recyclerView )
+    @Bind(R.id.recyclerView)
     RecyclerView recyclerView;
-    @Bind( R.id.fab )
+    @Bind(R.id.fab)
     FloatingActionButton floatingActionButton;
 
     private final List<Station> stationList = new ArrayList<>();
     private ItemTouchHelper itemTouchHelper;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_order );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
 
-        if ( getIntent() != null && getIntent().getBooleanExtra( EXTRA_DIALOG, false ) ) {
+        if (getIntent() != null && getIntent().getBooleanExtra(EXTRA_DIALOG, false)) {
             showAddDialog();
         }
 
@@ -76,53 +75,53 @@ public class OrderActivity extends BaseDragonActivity implements OnStartDragList
         setupNAvigationBar();
 
         recyclerView.setPadding(
-                0, DimenUtils.getStatusBarHeight( this, R.dimen.nav_bar_height ),
-                0, getResources().getDimensionPixelSize( R.dimen.item_air_quality_height ) * 3 );
+                0, DimenUtils.getStatusBarHeight(this, R.dimen.nav_bar_height),
+                0, getResources().getDimensionPixelSize(R.dimen.item_air_quality_height) * 3);
 
-        SmokSmogApplication.get( this )
+        SmokSmogApplication.get(this)
                 .getAppComponent()
-                .plus( new ActivityModule( this ) )
-                .inject( this );
+                .plus(new ActivityModule(this))
+                .inject(this);
 
-        OrderAdapter adapter = new OrderAdapter( stationList, this, settingsHelper );
+        OrderAdapter adapter = new OrderAdapter(stationList, this, settingsHelper);
 
-        recyclerView.setHasFixedSize( true );
-        recyclerView.setAdapter( adapter );
-        recyclerView.setLayoutManager( new LinearLayoutManager( this, VERTICAL, false ) );
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, VERTICAL, false));
 
-        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback( adapter );
-        itemTouchHelper = new ItemTouchHelper( callback );
-        itemTouchHelper.attachToRecyclerView( recyclerView );
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
 
         List<Long> stationIds = settingsHelper.getStationIdList();
 
         smokSmog.getApi().stations()
-                .subscribeOn( Schedulers.newThread() )
-                .flatMap( Observable::from )
-                .filter( station -> stationIds.contains( station.getId() ) )
-                .toSortedList( ( station, station2 ) -> stationIds.indexOf( station.getId() ) - stationIds.indexOf( station2.getId() ) )
-                .observeOn( AndroidSchedulers.mainThread() )
+                .subscribeOn(Schedulers.newThread())
+                .flatMap(Observable::from)
+                .filter(station -> stationIds.contains(station.getId()))
+                .toSortedList((station, station2) -> stationIds.indexOf(station.getId()) - stationIds.indexOf(station2.getId()))
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         stations -> {
                             stationList.clear();
-                            stationList.addAll( stations );
+                            stationList.addAll(stations);
                             recyclerView.getAdapter().notifyDataSetChanged();
                         },
                         throwable -> {
-                            logger.w( TAG, "Unable to build list", throwable );
-                        } );
+                            logger.w(TAG, "Unable to build list", throwable);
+                        });
 
     }
 
     private void setupFAB() {
-        int margin = getResources().getDimensionPixelSize( R.dimen.margin_16 );
+        int margin = getResources().getDimensionPixelSize(R.dimen.margin_16);
 
-        LayoutParams params = new LayoutParams( WRAP_CONTENT, WRAP_CONTENT );
+        LayoutParams params = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
 
         int bottomMargin = margin;
 
-        if ( getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ) {
-            bottomMargin += DimenUtils.getNavBarHeight( this );
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            bottomMargin += DimenUtils.getNavBarHeight(this);
         }
 
         params.bottomMargin = bottomMargin;
@@ -130,9 +129,9 @@ public class OrderActivity extends BaseDragonActivity implements OnStartDragList
         params.rightMargin = margin;
         params.gravity = Gravity.BOTTOM | Gravity.END;
         params.anchorGravity = Gravity.BOTTOM | Gravity.END;
-        params.setAnchorId( R.id.recyclerView );
+        params.setAnchorId(R.id.recyclerView);
 
-        floatingActionButton.setLayoutParams( params );
+        floatingActionButton.setLayoutParams(params);
     }
 
     @Override
@@ -141,50 +140,50 @@ public class OrderActivity extends BaseDragonActivity implements OnStartDragList
     }
 
     private void setupNAvigationBar() {
-        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN );
-        if ( getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ) {
-            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION );
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         }
     }
 
-    @OnClick( R.id.fab )
+    @OnClick(R.id.fab)
     void onClickFab() {
         showAddDialog();
     }
 
     private void showAddDialog() {
-        AddStationDialog.show( getSupportFragmentManager(), convertStationsToIdsArray( stationList ) );
+        AddStationDialog.show(getSupportFragmentManager(), StationUtils.Companion.convertStationsToIdsArray(stationList));
     }
 
     @Override
-    public void onStartDrag( RecyclerView.ViewHolder viewHolder ) {
-        itemTouchHelper.startDrag( viewHolder );
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        itemTouchHelper.startDrag(viewHolder);
     }
 
-    public static void start( Context context ) {
-        start( context, false );
+    public static void start(Context context) {
+        start(context, false);
     }
 
-    public static void start( Context context, boolean showDialog ) {
-        Intent intent = new Intent( context, OrderActivity.class );
-        intent.putExtra( EXTRA_DIALOG, showDialog );
-        context.startActivity( intent );
+    public static void start(Context context, boolean showDialog) {
+        Intent intent = new Intent(context, OrderActivity.class);
+        intent.putExtra(EXTRA_DIALOG, showDialog);
+        context.startActivity(intent);
     }
 
     @Override
-    public void onStation( long stationId ) {
-        smokSmog.getApi().station( stationId )
-                .subscribeOn( Schedulers.newThread() )
-                .observeOn( AndroidSchedulers.mainThread() )
+    public void onStation(long stationId) {
+        smokSmog.getApi().station(stationId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         station -> {
-                            stationList.add( station );
+                            stationList.add(station);
                             recyclerView.getAdapter().notifyDataSetChanged();
-                            settingsHelper.setStationIdList( convertStationsToIdsList( stationList ) );
+                            settingsHelper.setStationIdList(StationUtils.Companion.convertStationsToIdsList(stationList));
                         },
                         throwable -> {
-                            logger.e( TAG, "Unable to add station to station list", throwable );
-                            errorReporter.report( R.string.error_unable_to_add_station );
+                            logger.e(TAG, "Unable to add station to station list", throwable);
+                            errorReporter.report(R.string.error_unable_to_add_station);
                         }
                 );
     }
--- wear/src/main/java/com/antyzero/smoksmog/wear/NetworkModule.java
+++ wear/src/main/java/com/antyzero/smoksmog/wear/NetworkModule.java
@@ -1,11 +1,6 @@
 package com.antyzero.smoksmog.wear;
 
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-
-import java.util.Locale;
-
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -16,20 +11,9 @@ import pl.malopolska.smoksmog.SmokSmog;
 @Singleton
 public class NetworkModule {
 
-    private static final String ENDPOINT = "http://api.smoksmog.jkostrz.name/";
-
     @Provides
     @Singleton
-    public SmokSmog provideSmokSmog( Context context ) {
-        final SmokSmog.Builder builder = createBuilder( context );
-        builder.setDebug( BuildConfig.DEBUG );
-        return builder.build();
+    public SmokSmog provideSmokSmog() {
+        return new SmokSmog();
     }
-
-    @NonNull
-    protected SmokSmog.Builder createBuilder( Context context ) {
-        final Locale locale = context.getResources().getConfiguration().locale;
-        return new SmokSmog.Builder( ENDPOINT, locale );
-    }
-
 }
