cb757ac002a6bc6aa0273234acc237f1b7fab749
Iwo Polański <iwo.polanski@schibsted.pl>
2016-09-15 09:59:03 +0200

    Moving to Kotlin

---
+++ network/src/main/kotlin/pl/malopolska/smoksmog/Api.kt
@@ -0,0 +1,28 @@
+package pl.malopolska.smoksmog
+
+import pl.malopolska.smoksmog.model.Description
+import pl.malopolska.smoksmog.model.Station
+import retrofit.http.GET
+import retrofit.http.Path
+import rx.Observable
+
+interface Api {
+
+    @GET("/stations")
+    fun stations(): Observable<List<Station>>
+
+    @GET("/stations/{stationId}")
+    fun station(@Path("stationId") stationId: Long): Observable<Station>
+
+    @GET("/stations/{lat}/{lon}")
+    fun stationByLocation(@Path("lat") latitude: Double, @Path("lon") longitude: Double): Observable<Station>
+
+    @GET("/stations/{stationId}/history")
+    fun stationHistory(@Path("stationId") stationId: Long): Observable<Station>
+
+    @GET("/stations/{lat}/{lon}/history")
+    fun stationHistoryByLocation(@Path("lat") latitude: Double, @Path("lon") longitude: Double): Observable<Station>
+
+    @GET("/particulates/{id}/desc")
+    fun particulateDescription(@Path("id") particulateId: Long): Observable<Description>
+}
