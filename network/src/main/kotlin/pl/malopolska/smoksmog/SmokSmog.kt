aca629e1c68ada1a98308fd8d4c5f40493c6ad9b
Iwo Polański <outlying@gmail.com>
2016-12-20 16:10:59 +0100

    Added PM2,5 header

--- network/src/main/kotlin/pl/malopolska/smoksmog/SmokSmog.kt
+++ network/src/main/kotlin/pl/malopolska/smoksmog/SmokSmog.kt
@@ -1,8 +1,8 @@
 package pl.malopolska.smoksmog
 
 import com.fatboyindustrial.gsonjodatime.Converters
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import org.joda.time.DateTime
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
@@ -10,26 +10,42 @@ import retrofit2.converter.gson.GsonConverterFactory
 import rx.schedulers.Schedulers
 import java.util.*
 
-open class SmokSmog(locale: Locale = Locale.getDefault(), serverUrl: String = "http://api.smoksmog.jkostrz.name/") {
+open class SmokSmog(locale: Locale = Locale.getDefault(), serverUrl: String = ENDPOINT) {
 
-    constructor(locale: Locale = Locale.getDefault()) : this(locale, "http://api.smoksmog.jkostrz.name/")
+    constructor(locale: Locale = Locale.getDefault()) : this(locale, ENDPOINT)
 
-    val gson: Gson
+    constructor() : this(Locale.getDefault())
+
     val endpoint: String = "$serverUrl${locale.language}/"
+
     open val api: Api
 
     init {
 
-        val gsonBuilder = GsonBuilder()
-        gsonBuilder.registerTypeAdapter(DateTime::class.java, DateTimeDeserializer())
-        Converters.registerLocalDate(gsonBuilder)
-        gson = gsonBuilder.create()
+        val gsonBuilder = GsonBuilder().apply {
+            registerTypeAdapter(DateTime::class.java, DateTimeDeserializer())
+            Converters.registerLocalDate(this)
+        }
 
-        val builderRest = Retrofit.Builder()
-        builderRest.baseUrl(endpoint)
-        builderRest.addConverterFactory(GsonConverterFactory.create(gson))
-        builderRest.addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.newThread()))
-        val restAdapter = builderRest.build()
-        api = restAdapter.create(Api::class.java)
+        api = Retrofit.Builder().apply {
+            baseUrl(endpoint)
+            addConverterFactory(GsonConverterFactory.create(gsonBuilder.create()))
+            addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.newThread()))
+            client(OkHttpClient.Builder().apply {
+                addInterceptor { chain ->
+                    val original = chain.request()
+                    val request = original.newBuilder().apply {
+                        header("X-Smog-AdditionalMeasurements".toLowerCase(), "pm25")
+                        method(original.method(), original.body())
+                    }.build()
+                    chain.proceed(request)
+                }
+            }.build())
+        }.build().create(Api::class.java)
     }
+
+    companion object {
+        val ENDPOINT = "http://api.smoksmog.jkostrz.name/"
+    }
+
 }
