8a5595cd05b4b1c279e186366626515d54dc491a
Iwo Polański <outlying@gmail.com>
2017-01-03 15:06:23 +0100

    Mock correct

--- app/src/androidTest/java/com/antyzero/smoksmog/mock/MockNetworkModule.java
+++ app/src/androidTest/java/com/antyzero/smoksmog/mock/MockNetworkModule.java
@@ -2,11 +2,13 @@ package com.antyzero.smoksmog.mock;
 
 import com.antyzero.smoksmog.network.NetworkModule;
 
+import org.mockito.Mockito;
+
 import pl.malopolska.smoksmog.RestClient;
 
 public class MockNetworkModule extends NetworkModule {
 
     public RestClient provideSmokSmog() {
-        return new MockRestClient();
+        return Mockito.mock(RestClient.class);
     }
 }
