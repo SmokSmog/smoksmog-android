e225091bce8ee0d89c4945ac2f04be326c11dbbe
Iwo Polański <outlying@gmail.com>
2017-01-17 23:55:51 +0100

    Station list

--- network/src/test/java/pl/malopolska/smoksmog/RestClientTest.java
+++ network/src/test/java/pl/malopolska/smoksmog/RestClientTest.java
@@ -2,7 +2,13 @@ package pl.malopolska.smoksmog;
 
 import org.junit.Test;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
+
+import pl.malopolska.smoksmog.model.Station;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +26,22 @@ public class RestClientTest {
 
         // then
         assertThat(result).isEqualTo("http://api.smoksmog.jkostrz.name/" + locale.getLanguage() + "/");
+    }
+
+    @Test
+    public void stations() throws Exception {
+
+        List<Station> stations = new RestClient.Builder().build().stations().toBlocking().first();
+        List<String> names = new ArrayList<>();
+
+        for (Station station : stations) {
+            names.add(station.getName());
+        }
+
+        Collections.sort(names, Collator.getInstance());
+
+        for(String name : names){
+            System.out.print(name + ", ");
+        }
     }
 }
