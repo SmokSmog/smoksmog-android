2d043b0aa3c6c6942ab9e3204fe799cdee09b3ad
Iwo Polański <outlying@gmail.com>
2017-01-31 13:00:52 +0100

    Fixed problem with comparing

--- app/src/main/kotlin/com/antyzero/smoksmog/ui/screen/start/item/AirQualityViewHolder.kt
+++ app/src/main/kotlin/com/antyzero/smoksmog/ui/screen/start/item/AirQualityViewHolder.kt
@@ -72,8 +72,7 @@ class AirQualityViewHolder(itemView: View) : ListViewHolder<Station>(itemView) {
         val particulates = data.particulates
         if (!particulates.isEmpty()) {
 
-            val particulate = getNewest(particulates)
-            measureDate = particulate.date
+            measureDate = getNewest(particulates)?.date
             updateUiTime()
 
             // Loop update, we want up to date info
@@ -94,7 +93,7 @@ class AirQualityViewHolder(itemView: View) : ListViewHolder<Station>(itemView) {
      */
     private fun updateUiTime() {
         if (measureDate != null) {
-            val seconds = Seconds.secondsBetween(measureDate!!, DateTime.now()).seconds
+            val seconds = Seconds.secondsBetween(measureDate, DateTime.now()).seconds
             textViewMeasureTime.text = String.format(
                     itemView.resources.getText(R.string.measure_ago).toString(),
                     countdownProvider[seconds])
@@ -103,7 +102,10 @@ class AirQualityViewHolder(itemView: View) : ListViewHolder<Station>(itemView) {
         }
     }
 
-    private fun getNewest(data: List<Particulate>): Particulate {
-        return OperatorMinMax.max(Observable.from(data)) { lhs, rhs -> lhs.date.compareTo(rhs.date) }.toBlocking().first()
+    @Suppress("SENSELESS_COMPARISON")
+    private fun getNewest(data: List<Particulate>): Particulate? {
+        return OperatorMinMax.max(Observable.from(data).filter { it.date != null }) { lhs, rhs -> lhs.date.compareTo(rhs.date) }
+                .defaultIfEmpty(null)
+                .toBlocking().first()
     }
 }
