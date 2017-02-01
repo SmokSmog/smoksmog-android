03b9e5cc28f2931d909dbc8657ea22b9c0360cd6
Iwo Polański <outlying@gmail.com>
2017-02-01 16:59:38 +0100

    Correct quality index calculations, it's not linear

--- android/src/main/kotlin/smoksmog/air/AirQualityIndex.kt
+++ android/src/main/kotlin/smoksmog/air/AirQualityIndex.kt
@@ -27,7 +27,7 @@ object AirQualityIndex {
                 O3 -> particulateValue = (particulate.value / 120).toDouble()
                 PM10 -> particulateValue = (particulate.value / 100).toDouble()
                 PM25 -> particulateValue = (particulate.value / 60).toDouble()
-                C6H6 -> particulateValue = (particulate.value / 40).toDouble()
+                C6H6 -> particulateValue = calculateBenzene(particulate)
                 else -> {
                     // do nothing
                 }
@@ -37,5 +37,21 @@ object AirQualityIndex {
         }
 
         return index * 5
+    }
+
+    private fun calculateBenzene(particulate: Particulate): Double {
+        val value = particulate.value.toDouble()
+        val vp = (1 / (value * 5))
+        if (value <= 5) { // Index 0-1
+            return (value / 25)
+        } else if (value <= 10) { // Index 1-3
+            return (1 / (vp * (100 / 3) + (-1 / 3)))
+        } else if (value <= 15) { // Index 3-5
+            return (1 / (vp * 20 + (-1 / 15)))
+        } else if (value <= 20) { // Index 5-7
+            return (1 / (vp * (120 / 7) + (1 / 35)))
+        } else { // Index 7 and more
+            return (1 / (vp * (50 / 7) + (5 / 70))) // 25 - for index 7-10 and more
+        }
     }
 }
